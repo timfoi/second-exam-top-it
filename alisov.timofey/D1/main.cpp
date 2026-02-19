76fc35c21e2bfe42aeaa4c37c0dcf3cf6bb63527
AlisovTimofey <timofeyalisov@gmail.com>
2026-02-19 18:54:16 +0300

    start main

--- alisov.timofey/D1/main.cpp
+++ alisov.timofey/D1/main.cpp
@@ -2,4 +2,24 @@
 
 int main()
 {
+  char *ans = nullptr;
+  size_t len = 0, cap = 0;
+  size_t k;
+  char cr;
+
+  while (true)
+  {
+    if (!(std::cin >> k))
+    {
+      if (std::cin.eof())
+      {
+        break;
+      }
+      else
+      {
+        std::cerr << "Cant read 1 of parameters" << '\n';
+        return 1;
+      }
+    }
+  }
 }
